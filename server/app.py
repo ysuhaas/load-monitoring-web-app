4077f7637d14584bf7073abfea121a70780386b7
Suhaas Yerramreddy <ysuhaas@gmail.com>
2018-12-22 02:54:39 -0600

    Update a static page with CPU utilization using socketIO

--- server/app.py
+++ server/app.py
@@ -1,11 +1,34 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
+import time
+import threading
+import psutil
+from datetime import datetime
 
 app = Flask(__name__, static_folder='../static',
             template_folder='../static')
 
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
+
+cpu_util = psutil.cpu_percent()
+
+
+class publishThread(threading.Thread):
+    def __init__(self):
+        self.frequency = 1
+        super(publishThread, self).__init__()
+
+    def publishUtilization(self):
+        while True:
+            cpu_util = psutil.cpu_percent()
+            now = datetime.now()
+            print("Utilization at {} is {}".format(now.ctime(), str(cpu_util)))
+            socketio.emit('cpuUtil', {'util': cpu_util})
+            time.sleep(self.frequency)
+
+    def run(self):
+        self.publishUtilization()
 
 
 @app.route('/')
@@ -16,6 +39,8 @@ def index():
 @socketio.on('connect')
 def on_connect():
     print('Connected to client!')
+    thread = publishThread()
+    thread.start()
 
 
 @socketio.on('disconnect')
--- static/js/script.js
+++ static/js/script.js
@@ -2,4 +2,11 @@ $(document).ready(function(){
     // Connect to SocketIO server
     var socket = io.connect('http://' + document.domain + ':' + location.port);
     console.log('Connected to server!');
+
+    //receive details from server
+    socket.on('cpuUtil', function(msg) {
+        console.log("Received new utilization datapoint: " + msg.util);
+        data = '' + '<p>' + msg.util.toString() + '</p>';
+        $('#data').html(data);
+    });
 });
